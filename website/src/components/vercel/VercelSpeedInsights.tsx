b168bdb57268644c7dd1834234457967c1b58ff0
Huijie Wei <huijiewei@outlook.com>
2024-12-29 12:01:36 +0800

    chore: remove VercelSpeedInsights

--- website/src/components/vercel/VercelSpeedInsights.tsx
+++
@@ -1,9 +0,0 @@
-import type { SpeedInsightsProps } from "@vercel/speed-insights";
-import { SpeedInsights as SpeedInsightsScript } from "@vercel/speed-insights/react";
-import { useRoute } from "./utils";
-
-export const VercelSpeedInsights = (props: Omit<SpeedInsightsProps, "route">) => {
-  const route = useRoute();
-
-  return <SpeedInsightsScript route={route.route} {...props} framework="react-router" />;
-};
