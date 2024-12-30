bff9f027e1dc1caa2ff049ff54d1d9c4263fd2d8
Huijie Wei <huijiewei@outlook.com>
2024-12-30 08:26:37 +0800

    feat: add useMediaQuery hook

--- packages/react-ui/src/hooks/index.ts
+++ packages/react-ui/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from "./use-controllable-state";
 export * from "./use-isomorphic-effect";
+export * from "./use-media-query";
---
+++ packages/react-ui/src/hooks/use-media-query/index.ts
@@ -0,0 +1,25 @@
+import { useMemo, useSyncExternalStore } from "react";
+import { isBrowser } from "../../utils";
+
+export const useMediaQuery = (query: string) => {
+  const [getSnapshot, subscribe] = useMemo(() => {
+    const mediaQueryList = isBrowser ? window.matchMedia(query) : null;
+
+    return [
+      () => mediaQueryList?.matches ?? false,
+
+      (callback: () => void) => {
+        if (!mediaQueryList) {
+          return () => void 0;
+        }
+
+        mediaQueryList.addEventListener("change", callback);
+        return () => {
+          mediaQueryList.removeEventListener("change", callback);
+        };
+      },
+    ];
+  }, [query]);
+
+  return useSyncExternalStore(subscribe, getSnapshot, () => false);
+};
