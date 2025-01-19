56dcc5810f1db798aff84b92e028d7b02be1d918
Huijie Wei <huijiewei@outlook.com>
2025-01-19 22:34:57 +0800

    fix: fix PolymorphicProps cannot parse by react-docgen-typescript

--- packages/react-ui/src/primitives/polymorphic/index.ts
+++ packages/react-ui/src/primitives/polymorphic/index.ts
@@ -1,16 +1,16 @@
-import type { Assign, Simplify } from "@resolid/utils";
 import type { ComponentProps, ElementType, JSX } from "react";
 
 export type PrimitiveProps<
   T extends keyof JSX.IntrinsicElements,
   P extends Record<string, unknown> = Record<never, never>,
   O extends string | number | symbol = never,
-> = Simplify<Omit<Assign<JSX.IntrinsicElements[T], P>, O>>;
+> = Omit<Omit<JSX.IntrinsicElements[T], keyof P>, O> & P;
 
 export type PolymorphicProps<
   T extends ElementType,
   P extends Record<string, unknown> = Record<never, never>,
   O extends string | number | symbol = never,
-> = Omit<Assign<ComponentProps<T>, P>, O> & {
-  as?: T;
-};
+> = Omit<Omit<ComponentProps<T>, keyof P>, O> &
+  P & {
+    as?: T;
+  };
