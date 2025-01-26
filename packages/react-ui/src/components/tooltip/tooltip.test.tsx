09008a0ef96b55d78dfee0eb000c8582d1d57cba
Huijie Wei <huijiewei@outlook.com>
2025-01-26 09:07:01 +0800

    test: add test to Tooltip component

---
+++ packages/react-ui/src/components/tooltip/tooltip.test.tsx
@@ -0,0 +1,71 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { describe, expect, test } from "vitest";
+import { Tooltip, TooltipArrow, TooltipContent, TooltipTrigger } from "./tooltip";
+
+describe("Tooltip", () => {
+  test("renders tooltip trigger", () => {
+    render(
+      <Tooltip>
+        <TooltipTrigger>Tooltip Trigger</TooltipTrigger>
+        <TooltipContent>
+          <TooltipArrow />
+          Tooltip Content
+        </TooltipContent>
+      </Tooltip>,
+    );
+
+    expect(screen.getByText("Tooltip Trigger")).toBeInTheDocument();
+    expect(screen.queryByText("Tooltip Content")).not.toBeInTheDocument();
+  });
+
+  test("renders tooltip content when trigger is hovered", async () => {
+    render(
+      <Tooltip openDelay={0}>
+        <TooltipTrigger>Tooltip Trigger</TooltipTrigger>
+        <TooltipContent>
+          <TooltipArrow />
+          Tooltip Content
+        </TooltipContent>
+      </Tooltip>,
+    );
+
+    const trigger = screen.getByText("Tooltip Trigger");
+
+    expect(screen.queryByText("Tooltip Content")).not.toBeInTheDocument();
+
+    await userEvent.hover(trigger);
+
+    await waitFor(() => {
+      expect(screen.queryAllByText("Tooltip Content")[0]).toBeVisible();
+    });
+  });
+
+  test("renders tooltip content is dismissed when trigger is clicked", async () => {
+    render(
+      <Tooltip openDelay={0}>
+        <TooltipTrigger>Tooltip Trigger</TooltipTrigger>
+        <TooltipContent>
+          <TooltipArrow />
+          Tooltip Content
+        </TooltipContent>
+      </Tooltip>,
+    );
+
+    const trigger = screen.getByText("Tooltip Trigger");
+
+    expect(screen.queryByText("Tooltip Content")).not.toBeInTheDocument();
+
+    await userEvent.hover(trigger);
+
+    await waitFor(() => {
+      expect(screen.queryAllByText("Tooltip Content")[0]).toBeVisible();
+    });
+
+    await userEvent.click(trigger);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Tooltip Content")).not.toBeInTheDocument();
+    });
+  });
+});
