e69179dfbceb2bb19b9103a9fab71c9c5977236c
Huijie Wei <huijiewei@outlook.com>
2025-02-05 21:17:29 +0800

    fix: fix AvatarFallback color and size

--- packages/react-ui/src/components/avatar/avatar-fallback.tsx
+++ packages/react-ui/src/components/avatar/avatar-fallback.tsx
@@ -73,14 +73,15 @@ export const AvatarFallback = (props: PrimitiveProps<"div", AvatarFallbackProps,
       style={{ ...style, "--cv": color ? color.color : undefined } as CSSProperties}
       className={tx(
         "inline-flex h-full w-full items-center justify-center text-[calc(var(--sv)/3)] font-medium uppercase",
-        color ? ["bg-(--cv)", color.light ? "text-black" : "text-white"] : "bg-bg-muted",
+        color ? ["bg-(--cv)", color.light ? "text-black" : "text-white"] : !hasChildren && "bg-bg-muted",
+
         radiusClass,
         className,
       )}
       {...rest}
     >
       {children ?? initials ?? (
-        <svg viewBox="0 0 128 128" color="#fff">
+        <svg className={"h-full w-full"} color="#fff" viewBox="0 0 128 128">
           <path
             fill="currentColor"
             d="M103 102.139C93.094 111.92 79.35 118 64.164 118c-15.358 0-29.235-6.232-39.164-16.21V95.2C25 86.81 31.981 80 40.6 80h46.8c8.619 0 15.6 6.81 15.6 15.2v6.939zM63.996 24C51.294 24 41 34.294 41 46.996 41 59.706 51.294 70 63.996 70 76.7 70 87 59.706 87 46.996 87 34.294 76.699 24 63.996 24"
