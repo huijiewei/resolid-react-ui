219b92939f0f4c1d4cdaa3bb8799856774344330
Huijie Wei <huijiewei@outlook.com>
2025-03-05 13:21:59 +0800

    refactor(Badge): remove font-medium from base styles

--- packages/react-ui/src/components/badge/badge.styles.ts
+++ packages/react-ui/src/components/badge/badge.styles.ts
@@ -2,7 +2,7 @@ import { alertAndBadgeShareStyles } from "../../shared/styles";
 import { tcm, tv, type VP } from "../../utils";
 
 export const badgeStyles = tcm(
-  tv({ base: "inline-flex items-center rounded-md border px-2 py-1 text-xs font-medium" }),
+  tv({ base: "inline-flex items-center rounded-md border px-2 py-1 text-xs" }),
   alertAndBadgeShareStyles,
 );
 
