86e2311db8111aca7b36b65772c20aebaa1f7b13
Huijie Wei <huijiewei@outlook.com>
2025-09-26 08:28:54 +0800

    chore: add eslint-plugin-react-you-might-not-need-an-effect for improved linting

--- packages/react-ui/src/hooks/use-clipboard/index.ts
+++ packages/react-ui/src/hooks/use-clipboard/index.ts
@@ -10,6 +10,7 @@ export const useClipboard = (options?: UseClipboardOptions): readonly [boolean, 
 
   const [copied, setCopied] = useState(false);
 
+  // eslint-disable-next-line react-you-might-not-need-an-effect/no-reset-all-state-on-prop-change
   useEffect(() => {
     const id = copied ? setTimeout(() => setCopied(false), duration) : null;
 
